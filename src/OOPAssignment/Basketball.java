e2ecef8893fadbd9213bfd4d2ad7c819fb2aedc3
Alexyounggg <alex.young23@ycdsbk12.ca>
2022-11-13 16:14:00 -0500

    Created constructor and methdso for basketball

---
+++ src/OOPAssignment/Basketball.java
@@ -0,0 +1,21 @@
+package OOPAssignment;
+
+public class Basketball {
+
+    private Double size;
+    private String colour;
+
+    public Basketball(Double size, String colour) {
+        this.size = size;
+        this.colour = colour;
+    
+    }
+
+    public Double getSize() {
+        return this.size;
+    }
+
+    public String getColour() {
+        return this.colour;
+    }
+}

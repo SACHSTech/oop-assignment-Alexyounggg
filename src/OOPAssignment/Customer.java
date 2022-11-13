872a6d51761a6e224412c3951f3e91125ebe8e0d
Alexyounggg <alex.young23@ycdsbk12.ca>
2022-11-13 02:27:37 -0500

    Created constructor and added methods

--- src/OOPAssignment/Customer.java
+++ src/OOPAssignment/Customer.java
@@ -4,5 +4,31 @@ public class Customer{
 
     String firstName;
     String lastName;
+    Address address;
+    Boolean onApp;
+    Double moneyAmount;
+
+    public Customer (String firstName, String lastName, Address address, Boolean onApp, Double moneyAmount) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.onApp = onApp;
+        this.moneyAmount = moneyAmount;
+    }
     
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public Boolean getOnApp(){
+        return this.onApp;
+    }
+
+    public Double getMoney() {
+        return this.moneyAmount;
+    }
 }
